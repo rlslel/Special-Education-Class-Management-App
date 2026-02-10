3b768261c15ac6c71a84c3b8ce526980be47d430
lee <djqan4631@gmail.com>
2026-02-10 20:31:11 +0900

    배포 전 설정

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Special-Education-Class-Management-App/",  // <--- 이 부분을 꼭 추가하세요! (앞뒤로 슬래시 / 필수)
 })
